cf59b6b42565249daf924a7d5754bf450f32fc7d
Pingosoft <fernandokulaif@gmail.com>
2021-05-19 14:12:23 -0400

    correcting header.php wp_head(); no need of <title>

--- footer-splash.php
+++ footer-splash.php
@@ -1,6 +1,7 @@
 <div class="notice">
-    <p>This is the footer - (footer-splash.php)</p>
+    <p>This is the custom footer - (footer-splash.php)</p>
 </div>
+<?php wp_footer(); ?>
 </body>
 
 </html>
